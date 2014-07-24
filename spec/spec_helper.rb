ac5a673d6c7fd04677a13ddd0cd2cca4e48815f5
Parker Moore <parkrmoore@gmail.com>
2014-07-24 16:22:41 -0400

    Upgrade to Rspec 3.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../lib/github-pages-health-check.rb", __FILE__)
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.raise_errors_for_deprecations!
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
