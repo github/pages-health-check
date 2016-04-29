524895be34c2c6596f4737acdee44bcc337c93de
Ben Balter <ben.balter@github.com>
2016-04-29 13:13:07 -0400

    better error specs

--- lib/github-pages-health-check/error.rb
+++ lib/github-pages-health-check/error.rb
@@ -2,6 +2,7 @@ module GitHubPages
   module HealthCheck
     class Error < StandardError
       DOCUMENTATION_BASE = "https://help.github.com"
+      DOCUMENTATION_PATH = "/categories/github-pages-basics/"
 
       attr_reader :repository, :domain
 
@@ -19,9 +20,16 @@ module GitHubPages
         @subclasses ||= []
       end
 
-      def message_formatted
-        "#{message.gsub(/\s+/, " ").strip} #{more_info}"
+      def message
+        "Something's wrong with your GitHub Pages site."
       end
+
+      def message_with_url
+        msg = message.gsub(/\s+/, " ").squeeze(" ").strip
+        msg << "." unless msg =~ /\.$/
+        [msg, more_info].join(" ")
+      end
+      alias_method :message_formatted, :message_with_url
 
       private
 
