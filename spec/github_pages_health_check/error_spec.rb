7960264a8607b5381813659cdf4d3d0a037a3f70
Parker Moore <parkr@github.com>
2018-04-25 15:15:44 -0400

    Remove String#+ uses since that's also not in Ruby 2.2

--- spec/github_pages_health_check/error_spec.rb
+++ spec/github_pages_health_check/error_spec.rb
@@ -50,15 +50,15 @@ RSpec.describe(GitHubPages::HealthCheck::Error) do
   end
 
   it "builds the more info string" do
-    msg = +"For more information, "
-    msg << "see https://help.github.com/categories/github-pages-basics/."
+    msg = "For more information, " \
+      "see https://help.github.com/categories/github-pages-basics/."
     expect(subject.send(:more_info)).to eql(msg)
   end
 
   it "returns the message with URL" do
-    msg = +"Something's wrong with your GitHub Pages site. "
-    msg << "For more information, "
-    msg << "see https://help.github.com/categories/github-pages-basics/."
+    msg = "Something's wrong with your GitHub Pages site. " \
+      "For more information, " \
+      "see https://help.github.com/categories/github-pages-basics/."
     expect(subject.message_with_url).to eql(msg)
   end
 end
