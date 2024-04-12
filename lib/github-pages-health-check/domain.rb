d67d170d3439a98f44d1a99e73fdc3d57fedece4
tsusdere <tsusdere@github.com>
2024-04-12 13:38:55 -0500

    add check for domain name

--- lib/github-pages-health-check/domain.rb
+++ lib/github-pages-health-check/domain.rb
@@ -410,8 +410,8 @@ module GitHubPages
         cnames = dns.take_while { |answer| answer.type == Dnsruby::Types::CNAME }
         return if cnames.empty?
 
-        # check to see if the CNAME starts with www
-        @wwwcname ||= cnames.last.name.to_s.start_with?("www")
+        # check to see if the CNAME starts with www and domain name is the same
+        @wwwcname ||= cnames.last.name.to_s.start_with?("www") && cnames.last.name.to_s.end_with?(cnames.last.domainname.to_s)
         @cname ||= Domain.new(cnames.last.cname.to_s)
       end
 
